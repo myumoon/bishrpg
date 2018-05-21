3b46405b63d1e97071c7a2e61eb707b3ebda8570
ryunosuke_ide <ryumyu0120@gmail.com>
2018-05-22 02:59:56 +0900

    feat:バトルシステム実装途中　ダメージ計算まで

--- bishrpg/Source/bishrpg/Battle/BattleSystem.cpp
+++ bishrpg/Source/bishrpg/Battle/BattleSystem.cpp
@@ -18,7 +18,7 @@ UBattleSystem::UBattleSystem()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = false;
 
-
+	CommandList.Reserve(32); // 12マスなので余裕を持ってこのくらい
 }
 
 
@@ -67,13 +67,13 @@ FBattleCharacterStatus UBattleSystem::MakeBattleCharacterStatusWithOffset(const 
 		return curve ? static_cast<int32>(curve->GetFloatValue(static_cast<float>(lv))) : defaultValue;
 	};
 
-	battleStatus.Style    = charData ? charData->Style : EBattleStyle::Humor;
+	battleStatus.Hate     = 0;	battleStatus.Style    = charData ? charData->Style : EBattleStyle::Humor;
 	battleStatus.HpMax    = getCurveFrom(hpTbl, stat.HpLv, 1) + offsetHp;
 	battleStatus.Hp       = battleStatus.HpMax;
 	battleStatus.Attack   = getCurveFrom(attackTbl, stat.AttackLv, 1) + offsetAttack;
 	battleStatus.Deffence = getCurveFrom(deffenceTbl, stat.DeffenceLv, 1) + offsetDeffence;
 	battleStatus.Speed    = getCurveFrom(speedTbl, stat.SpeedLv, 1) + offsetSpeed;
-	battleStatus.Hate     = 0;
+
 
 	return battleStatus;
 }
@@ -96,4 +96,151 @@ FBattleParty UBattleSystem::MakeFromParty(const FParty& party)
 // 初期化
 void UBattleSystem::Initialize(const FParty& playerParty, const FParty& opponentParty, int32 aaa)
 {
+	const FParty* partyList[] = { &playerParty, &opponentParty };
+
+	for(auto* party : partyList) {
+		FBattleParty battleParty = MakeFromParty(*party);
+		PartyList.Add(battleParty);
+	}
 }
+
+// 攻撃コマンド追加
+bool UBattleSystem::PushAttackCommand(int32 posIndex)
+{
+	FBattleCommand addCommand;
+	addCommand.ActionType     = EBattleActionType::Attack;
+	addCommand.PosIndex       = posIndex;
+	addCommand.TargetPosIndex = 0;
+	CommandList.Push(addCommand);
+
+	return CanStartCommand();
+}
+
+// スキルコマンド追加
+bool UBattleSystem::PushSkillCommand(int32 posIndex, const FName& skillName)
+{
+	FBattleCommand addCommand;
+	addCommand.ActionType      = EBattleActionType::Skill;
+	addCommand.PosIndex        = posIndex;
+	addCommand.TargetPosIndex  = 0;
+	addCommand.SkillName       = skillName;
+	CommandList.Push(addCommand);
+
+	return CanStartCommand();
+}
+
+// 移動コマンド追加
+bool UBattleSystem::PushMoveCommand(int32 posIndex, int32 moveTo)
+{
+	FBattleCommand addCommand;
+	addCommand.ActionType     = EBattleActionType::Move;
+	addCommand.PosIndex       = posIndex;
+	addCommand.TargetPosIndex = moveTo;
+	CommandList.Push(addCommand);
+
+	return CanStartCommand();
+}
+
+// 巻き戻す
+bool UBattleSystem::RevertCommand(FBattleCommand& revertedCommand)
+{
+	if(CommandList.Num() == 0) {
+		return false;
+	}
+
+	revertedCommand = CommandList.Last();
+	CommandList.RemoveAt(CommandList.Num() - 1);
+
+	return true;
+}
+
+// リセット
+void UBattleSystem::ResetCommand()
+{
+	CommandList.Reset();
+}
+
+// コマンド実行可能か
+int32 UBattleSystem::CalcAlivePlayers() const
+{
+	int32 aliveCount = 0;
+
+	const auto& party = PartyList[PartyIndex::Player];
+	for(auto pos : party.Formation) {
+		if(0 < party.Characters[pos].Hp) {
+			++aliveCount;
+		}
+	}
+	return aliveCount;
+}
+
+// コマンド実行可能か
+bool UBattleSystem::CanStartCommand() const
+{
+	return (CalcAlivePlayers() == CommandList.Num());
+}
+
+// コマンド実行
+bool UBattleSystem::ConsumeCommand(FBattleActionResult& result)
+{
+	if(CommandList.Num() == 0) {
+		return false;
+	}
+
+	FBattleCommand execCommand = CommandList.Pop();
+	result.ActionType = execCommand.ActionType;
+
+	switch(execCommand.ActionType) {
+		case EBattleActionType::Attack : {
+			ExecAttack(result, execCommand);
+		} break;
+
+		case EBattleActionType::Move : {
+			ExecMove(result, execCommand);
+		} break;
+
+		case EBattleActionType::Skill : {
+			ExecSkill(result, execCommand);
+		} break;
+
+		default:
+			break;
+	}
+
+	return true;
+}
+
+// 攻撃
+void UBattleSystem::ExecAttack(FBattleActionResult& result, const FBattleCommand& command)
+{
+	auto& attackChar = GetCharacterByPos(GetTurnParty(), command.PosIndex);
+	const int32 targetPos  = 0;
+	auto& targetChar = GetCharacterByPos(GetNotTurnParty(), targetPos);
+	const float attack     = attackChar.Attack;
+	const float deffence   = targetChar.Deffence;
+
+	// 攻撃-防御
+	const float attackDiff = FMath::Max(attack - deffence, 0.0f);
+
+	// 攻撃力に加算する
+	const int32 rand       = FMath::RandRange(-50, 50);
+
+	// 攻撃-防御の差が開くほどダメージが大きくなるようにする
+	const float damageBase = FMath::Pow(attackDiff * 0.01f, 1.3f) * (attack + rand);
+
+	// 最低ダメージ保証
+	const float damage     = FMath::Max(damageBase, 10.0f);
+}
+
+// スキル
+void UBattleSystem::ExecSkill(FBattleActionResult& result, const FBattleCommand& command)
+{
+	
+}
+
+// 移動
+void UBattleSystem::ExecMove(FBattleActionResult& result, const FBattleCommand& command)
+{
+
+}
+
--- bishrpg/Source/bishrpg/Battle/BattleSystem.h
+++ bishrpg/Source/bishrpg/Battle/BattleSystem.h
@@ -15,9 +15,7 @@ enum class EBattleActionType : uint8
 {
 	Attack,    //!< 攻撃
 	Skill,     //!< スキル
-	Heal,      //!< 回復
-	Spawn,     //!< 空きマスに召喚
-	Move,
+	Move,      //!< 移動
 };
 
 /*! ターゲット
@@ -127,17 +125,30 @@ struct FBattleParty {
 
 };
 
+/*! バトルコマンド
+*/
+USTRUCT(BlueprintType)
+struct FBattleCommand {
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battle")
+	EBattleActionType ActionType;  //!< 行動
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battle")
+	int32 PosIndex;        //!< マス目
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battle")
+	int32 TargetPosIndex;  //!< 対象
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Battle")
+	FName SkillName;       //!< 発動スキル
+};
+
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BISHRPG_API UBattleSystem : public UActorComponent
 {
 	GENERATED_BODY()
-
-public:	
-	// Sets default values for this component's properties
-	UBattleSystem();
-
-
 
 private:
 	enum PartyIndex {
@@ -147,11 +158,10 @@ private:
 		PartyNum,
 	};
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-
 public:	
+	// Sets default values for this component's properties
+	UBattleSystem();
+
 	// Called every frame
 	//virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
@@ -160,16 +170,116 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Battle")
 	void Initialize(const FParty& playerParty, const FParty& opponentParty, int32 aaa);
 
+	/*!	バトル行動を追加
+		@return コマンド数がいっぱいになったらtrue
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool PushSkillCommand(int32 posIndex, const FName& skillName);
+
+	/*!	バトル行動を追加
+		@return コマンド数がいっぱいになったらtrue
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool PushMoveCommand(int32 posIndex, int32 moveTo);
+
+	/*!	バトル行動を追加
+		@return コマンド数がいっぱいになったらtrue
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool PushAttackCommand(int32 posIndex);
+
+	/*!	バトル行動を戻す
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool RevertCommand(FBattleCommand& revertedCommand);
+
+	/*!	バトル行動を除去
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	void ResetCommand();
+
+	/*!	バトル開始可能かどうか
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool CanStartCommand() const;
+
+	/*!	ターン設定
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	void SetTurn(bool playerTurn) { PlayerTurn = playerTurn; }
+
+	/*!	ターン設定
+		@return プレイヤーターンかどうか
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool SwapTurn() { PlayerTurn = !PlayerTurn; return PlayerTurn; }
+
+	/*!	行動を行う
+		@param[out] result 計算結果
+		@return     消費するコマンドが合った場合はtrue
+	*/
+	UFUNCTION(BlueprintCallable, Category = "Battle")
+	bool ConsumeCommand(FBattleActionResult& result);
+
+	/*!	バトル用のキャラステータス生成
+	*/
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Battle")
 	static FBattleCharacterStatus MakeBattleCharacterStatus(const FCharacterStatus& stat);
 
+	/*!	バトル用のキャラステータス生成（数値オフセット付き）
+	*/
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Battle")
 	static FBattleCharacterStatus MakeBattleCharacterStatusWithOffset(const FCharacterStatus& stat, int32 offsetHp, int32 offsetAttack, int32 offsetDeffence, int32 offsetSpeed);
 	
+	/*!	バトル用パーティ生成
+	*/
 	UFUNCTION(BlueprintCallable, BlueprintPure, Category = "Battle")
 	static FBattleParty MakeFromParty(const FParty& party);
 
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	/*!	バトル行動を追加
+	*/
+	//UFUNCTION(BlueprintCallable, Category = "Battle")
+	//void PushAction(const FBattleCommand& command);
+
+	/*!	生存しているプレイヤー数
+	*/
+	int32 CalcAlivePlayers() const;
+
+	/*!	攻撃コマンド実行
+	*/
+	void ExecAttack(FBattleActionResult& result, const FBattleCommand& command);
+
+	/*!	移動コマンド実行
+	*/
+	void ExecMove(FBattleActionResult& result, const FBattleCommand& command);
+
+	/*!	スキルコマンド実行
+	*/
+	void ExecSkill(FBattleActionResult& result, const FBattleCommand& command);
+
+	/*!	ターンのパーティーを取得
+	*/
+	FBattleParty& GetTurnParty() { return PartyList[PlayerTurn ? 0 : 1]; }
+
+	/*!	ターンでないパーティーを取得
+	*/
+	FBattleParty& GetNotTurnParty() { return PartyList[PlayerTurn ? 1 : 0]; }
+
+	/*!	指定場所のキャラを取得
+	*/
+	FBattleCharacterStatus& GetCharacterByPos(FBattleParty& party, int32 posIndex) const
+	{
+		check(posIndex < party.Formation.Num());
+		check(party.Formation[posIndex] < party.Characters.Num());
+		return party.Characters[party.Formation[posIndex]];
+	}
 
 private:
-	TArray<FBattleParty> PartyList;
+	TArray<FBattleParty>   PartyList;   //!< パーティ
+	TArray<FBattleCommand> CommandList; //!< バトルコマンドリスト
+	bool                   PlayerTurn = true;  //!< プレイヤーターンかどうか
 };
