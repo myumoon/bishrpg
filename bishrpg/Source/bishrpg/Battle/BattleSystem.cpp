b36721b8688e31a55ddaae20f3742e193a87179e
ryunosuke_ide <ryumyu0120@gmail.com>
2018-10-14 20:10:13 +0900

    refactor:BattleBoardUtilを別ファイルにする

--- bishrpg/Source/bishrpg/Battle/BattleSystem.cpp
+++ bishrpg/Source/bishrpg/Battle/BattleSystem.cpp
@@ -11,51 +11,6 @@
 #include "bishrpg.h"
 
 
-void UBattleBoardUtil::MakePositionListCol(TArray<int32>& madePosList, int32 col, bool up)
-{
-	if(UBattleBoardUtil::GetBoardCol() <= col) {
-		GAME_ERROR("invalid col %d", col);
-		return;
-	}
-
-	// 後衛から
-	if(up) {
-		for(int32 i = col; i < UBattleBoardUtil::GetCellNum(); i += UBattleBoardUtil::GetBoardCol()) {
-			madePosList.Add(i);
-		}
-	}
-	// 前線から
-	else {
-		for(int32 i = UBattleBoardUtil::GetCellNum() - UBattleBoardUtil::GetBoardCol() + col; 0 <= i; i -= UBattleBoardUtil::GetBoardCol()) {
-			madePosList.Add(i);
-		}
-	}
-}
-
-void UBattleBoardUtil::MakePositionListRow(TArray<int32>& madePosList, int32 row)
-{
-	const int32 begin = row * UBattleBoardUtil::GetBoardRow();
-	const int32 end   = begin + UBattleBoardUtil::GetBoardRow();
-	for(int32 i = begin; i < end; ++i) {
-		madePosList.Add(i);
-	}
-}
-void UBattleBoardUtil::MakePositionListRandom(TArray<int32>& madePosList, int32 positions, const FRandomStream& randStream)
-{
-	TArray<int32> poslist;
-	poslist.Reserve(CELL_NUM);
-	for(int32 i = 0; i < CELL_NUM; ++i) {
-		poslist.Add(i);
-	}
-
-	for(int32 i = 0; i < positions; ++i) {
-		const int32 index = randStream.RandRange(0, poslist.Num());
-		madePosList.Add(poslist[index]);
-		poslist.RemoveAt(index, 1, false);
-	}
-}
-
-
 
 
 
