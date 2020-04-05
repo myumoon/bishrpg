5900fb2c637cebc2659bccb706c95caf5e08c58f
myumoon <ryumyu0120@gmail.com>
2020-04-05 14:31:58 +0900

    feat プロジェクトディレクトリを取得できるようにする

--- tools/bin/project_def.bat
+++ tools/bin/project_def.bat
@@ -3,7 +3,10 @@ rem =======================================================
 rem プロジェクト情報
 rem =======================================================
 
+set UE4_DIR="D:\Program Files\Epic Games\UE_4.24\Engine\Binaries\Win64"
+
 set PROJECT_ROOT_DIR=%~dp0\..\..\
+set UE4_PROJECT_DIR=%PROJECT_ROOT_DIR%\bishrpg\
 
 set ASSET_DIR=D:\prog\0_myprogram\bishrpg_resources
 
